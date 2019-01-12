9ab8b7e427fb57ba278d14635113cf0309f41e40
luckylucy717 <luckylucy0717@gmail.com>
2019-01-12 17:38:28 +0900

    패키지 정리

--- src/algorithm/basic/p2490.java
+++ src/algorithm/basic/p2490.java
@@ -1,4 +1,4 @@
-package algorithm;
+package algorithm.basic;
 
 import java.util.Arrays;
 import java.util.List;
--- src/algorithm/queue/CircularQueue.java
+++ src/algorithm/queue/CircularQueue.java
@@ -1,4 +1,4 @@
-package algorithm;
+package algorithm.queue;
 
 import java.util.Arrays;
 
